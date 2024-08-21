83c04e9320b17ebf6e0c6ef77d9b0180a5416177
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-08-21 08:43:55 -0400

    Estructura inicial del código

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+class juegoBlackjack {
+
+    public static void main(String[] args) {
+        crearMatrizCartas();
+        agregarCartas();
+        inicializarJuego();
+        obtenerCartas();
+        jugarJuego();
+        salir();
+        menu();
+
+    }
+
+}
